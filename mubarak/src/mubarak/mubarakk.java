350d32bbc2b047c30f8c95754f4becd9f8d4a714
cp <cp@LAPTOP-9HR5B376>
2021-02-18 05:59:23 +0300

    First commit

---
+++ mubarak/src/mubarak/mubarakk.java
@@ -0,0 +1,11 @@
+package mubarak;
+
+public class mubarakk {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("My Name is mubarak");
+		
+	}
+
+}
